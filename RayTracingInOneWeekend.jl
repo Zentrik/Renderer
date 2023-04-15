039ab9fdccf79115a94bd0793d35328bdfc4b598
zentrik <Zentrik@users.noreply.github.com>
2023-04-15 01:22:25 +0100

    Switch back to using Sum Types for Material

--- RayTracingInOneWeekend.jl
+++ RayTracingInOneWeekend.jl
@@ -1,12 +1,12 @@
 # This tries to stay faithful to the book's code
 
-using Parameters, StaticArrays, LinearAlgebra, Images, ThreadsX, FunctionWrappers, SIMD, StructArrays, Polyester, Virtual
+using Parameters, StaticArrays, LinearAlgebra, Images, ThreadsX, FunctionWrappers, SIMD, StructArrays, Polyester, MLStyle
+using Expronicon.ADT: @adt
 
 const T = Float32
 const N = 8 # vector width
 
-const Point = SVector{3, T} # We use define Float64 so we dont have points of different types, otherwise Ray, Sphere become parametric types and HittableList needs to be contructed carefully to ensure same types everywhere. (can we somehow promote it)
-# we dont do const T = Float64 as that makes Point(bla) slow in sample_hemisphere and world() slow. # seems fine now? i think splitting sample_hemisphere into sample_sphere has helpled.
+const Point = SVector{3, T} # We use T so we dont have points of different types, otherwise Ray, Sphere become parametric types and HittableList needs to be contructed carefully to ensure same types everywhere. (can we somehow promote it)
 const Spectrum = SVector{3, T}
 
 @with_kw struct Ray @deftype Point
@@ -16,34 +16,35 @@ const Spectrum = SVector{3, T}
 end
 @inline @fastmath (ray::Ray)(t) = ray.origin + t * ray.direction
 
-# struct Material
-#     attenuation::Spectrum
-#     scatter::FunctionWrapper{Point, Tuple{Ray, Point}}
-# end
-
-abstract type Material end
-struct Lambertian <: Material
-end
-@with_kw struct Dielectric <: Material
-    ior::T = 3//2
-end
-@with_kw struct Metal <: Material
-    fuzz::T = 0
+@adt Material begin
+    struct Lambertian
+        attenuation::Spectrum = ones(Spectrum)
+    end
+    struct Dielectric
+        attenuation::Spectrum = ones(Spectrum)
+        ior::T = 3//2
+    end
+    struct Metal
+        attenuation::Spectrum = ones(Spectrum)
+        fuzz::T = 0
+    end
 end
 
 struct hit_record
     p::Point
     normal::Point
+    material::Material
     t::T
 end
 
 abstract type Primitive end
 
 @with_kw struct Sphere <: Primitive
-    centre::Point = zeros(Point)
-    radius::T = 1//2
-    attenuation::Spectrum = ones(Spectrum)
-    material::Material = Lambertian()
+    centre::Point
+    radius::T
+    material::Material
+
+    Sphere(centre=zeros(Point), radius=1//2, material=Material.Lambertian()) = new(centre, radius, material)
 end
 
 @fastmath sphere_normal(sphere, position) = (position - sphere.centre) / sphere.radius
@@ -187,14 +188,6 @@ end
     return direction
 end
 
-# glass(ior=3//2) = (ray, n⃗) -> glass(ray, n⃗, ior)
-# metal(fuzz=0) = (ray, n⃗) -> reflect(ray, n⃗, fuzz) # is it slow?
-
-@virtual scatter(ray, n⃗, material::Material) = error()
-@override scatter(ray, n⃗, material::Dielectric) = glass(ray, n⃗, material.ior)
-@override scatter(ray, n⃗, material::Lambertian) = lambertian(ray, n⃗)
-@override scatter(ray, n⃗, material::Metal) = reflect(ray, n⃗, material.fuzz)
-
 @generated function getBits(mask::SIMD.Vec{N, Bool}) where N #This reverses the bits
     s = """
     %mask = trunc <$N x i8> %0 to <$N x i1>
@@ -232,8 +225,7 @@ end
     return ccall("llvm.x86.avx.vtestz.ps.256", llvmcall, Int32, (SIMD.LVec{8, Float32}, SIMD.LVec{8, Float32}), y, y) == 0
 end
 
-const initialSphere = Sphere()
-const initialRecord = hit_record(zeros(Point), normalize(ones(Point)), Inf)
+const initialRecord = hit_record(zeros(Point), normalize(ones(Point)), Sphere().material, Inf)
 
 @fastmath function findSceneIntersection(r, hittable_list, tmin, tmax)
     hitT = SIMD.Vec{N, T}(tmax)
@@ -280,9 +272,9 @@ const initialRecord = hit_record(zeros(Point), normalize(ones(Point)), Inf)
         @inbounds sphere = hittable_list.Sphere[i]
         normal = sphere_normal(sphere, position)
 
-        return sphere, hit_record(position, normal, minHitT)
+        return hit_record(position, normal, sphere.material, minHitT)
     else 
-        return initialSphere, initialRecord
+        return initialRecord
     end
 end
 
@@ -290,22 +282,22 @@ end
     accumulated_attenuation = ones(Spectrum)
 
     for _ in 1:depth
-        sphere, record = findSceneIntersection(r, world, tmin, tmax)
+        record = findSceneIntersection(r, world, tmin, tmax)
 
         if record.t == tmax # nothing hit, t from initialRecord
             # @assert all(world_color(r) .>= 0)
             return accumulated_attenuation .* world_color(r)
         else
             # # @assert norm(record.normal) ≈ 1
-            direction = scatter(r, record.normal, sphere.material)
+            # direction = scatter(r, record.normal, sphere.material)
             # # @assert norm(direction) ≈ 1
-            attenuation = sphere.attenuation
+            # attenuation = sphere.attenuation
 
-            # @fastmath @inline (direction, attenuation) = @match record.material begin
-            #     Material.Lambertian(attenuation) => (lambertian(r, record.normal), attenuation)
-            #     Material.Dielectric(attenuation, ior) => (glass(r, record.normal, ior), attenuation)
-            #     Material.Metal(attenuation, fuzz) => (reflect(r, record.normal, fuzz), attenuation)
-            # end
+            @fastmath @inline (direction, attenuation) = @match record.material begin
+                Material.Lambertian(attenuation) => (lambertian(r, record.normal), attenuation)
+                Material.Dielectric(attenuation, ior) => (glass(r, record.normal, ior), attenuation)
+                Material.Metal(attenuation, fuzz) => (reflect(r, record.normal, fuzz), attenuation)
+            end
 
             r = Ray(record.p, direction)
             accumulated_attenuation = accumulated_attenuation .* attenuation
@@ -317,7 +309,7 @@ end
 
 function scene_random_spheres()
 	# HittableList = Sphere[] # SVector{486, Sphere} #  # StructArrays{Sphere} #
-    HittableList = [Sphere([0, 0, -1000], 1000, [.5, .5, .5], Lambertian())]
+    HittableList = [Sphere([0, 0, -1000], 1000, Material.Lambertian([.5, .5, .5]))]
 
 	for a in -11:10, b in -11:10
 		choose_mat = rand()
@@ -329,24 +321,24 @@ function scene_random_spheres()
 		if choose_mat < 4//5
 			# lambertian
 			albedo = rand(Spectrum) .* rand(Spectrum)
-			push!(HittableList, Sphere(center, 1//5, albedo, Lambertian()))
+			push!(HittableList, Sphere(center, 1//5, Material.Lambertian(albedo)))
 		elseif choose_mat < 95//100
 			# metal
 			albedo = rand(Spectrum) / 2 .+ 1/2
 			fuzz = rand() * 5
-			push!(HittableList, Sphere(center, 0.2, albedo, Metal(fuzz)))
+			push!(HittableList, Sphere(center, 0.2, Material.Metal(albedo, fuzz)))
 		else
 			# glass
-			push!(HittableList, Sphere(center, 0.2, ones(Spectrum), Dielectric()))
+			push!(HittableList, Sphere(center, 0.2, Material.Dielectric()))
 		end
 	end
 
-	push!(HittableList, Sphere([0,0,1], 1, ones(Spectrum), Dielectric()))
-	push!(HittableList, Sphere([-4,0,1], 1, [0.4,0.2,0.1], Lambertian()))
-	push!(HittableList, Sphere([4,0,1], 1, [0.7,0.6,0.5], Metal(0)))
+	push!(HittableList, Sphere([0,0,1], 1, Material.Dielectric()))
+	push!(HittableList, Sphere([-4,0,1], 1, Material.Lambertian([0.4,0.2,0.1])))
+	push!(HittableList, Sphere([4,0,1], 1, Material.Metal([0.7,0.6,0.5], 0)))
 
-    append!(HittableList, repeat([Sphere(zeros(Point), 0, ones(Spectrum), Lambertian())], (N - mod1(length(HittableList), N))))
-    tmp = StructArray(HittableList, unwrap = T -> (T<:AbstractVector))::StructVector{Sphere, NamedTuple{(:centre, :radius, :attenuation, :material), Tuple{StructVector{SVector{3, Float32}, NamedTuple{(:x, :y, :z), Tuple{Vector{Float32}, Vector{Float32}, Vector{Float32}}}, Int64}, Vector{Float32}, StructVector{SVector{3, Float32}, NamedTuple{(:x, :y, :z), Tuple{Vector{Float32}, Vector{Float32}, Vector{Float32}}}, Int64}, Vector{Material}}}, Int64}
+    append!(HittableList, repeat([Sphere(zeros(Point), 0)], (N - mod1(length(HittableList), N))))
+    tmp = StructArray(HittableList, unwrap = T -> (T<:AbstractVector))::StructVector{Sphere, NamedTuple{(:centre, :radius, :material), Tuple{StructVector{SVector{3, Float32}, NamedTuple{(:x, :y, :z), Tuple{Vector{Float32}, Vector{Float32}, Vector{Float32}}}, Int64}, Vector{Float32}, Vector{Material}}}, Int64}
     return hittable_list(tmp);
 end
 
