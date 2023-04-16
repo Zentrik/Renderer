87eab55b9d868407f39630bc1335cbb60a4eba9f
Zentrik <Zentrik@users.noreply.github.com>
2023-04-16 16:48:10 +0100

    Fix Benchmark

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -5,4 +5,4 @@ const SUITE = BenchmarkGroup()
 
 SUITE["RayTracingInOneWeekend"] = BenchmarkGroup(["test"])
 
-SUITE["RayTracingInOneWeekend"]["render"] = @benchmarkable Renderer.render!(x..., samples_per_pixel=5, parallel=false) setup=(x=Renderer.setup(1920/2))
+SUITE["RayTracingInOneWeekend"]["1920/2p with 5 samples per pixel and max depth of 16"] = @benchmarkable Renderer.render!(x[2], x[1], x[3], samples_per_pixel=5, parallel=false) setup=(x = Renderer.setup(1920/2))
