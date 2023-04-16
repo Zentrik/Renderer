52b4ee4275bf7b278530e241adae99e42f0b84a0
Zentrik <Zentrik@users.noreply.github.com>
2023-04-16 15:33:03 +0100

    Add SmartAsserts and remove Fast.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,17 @@
-using Renderer, Test
+using Renderer, Test, SmartAsserts
 
 @testset "Slow" begin
-    @test_nowarn Renderer.Slow.test(false)
-    @test_nowarn Renderer.Slow.test()
+    @test_nowarn Renderer.test(false)
+    @test_nowarn Renderer.test()
 end
 
-@testset "Fast" begin
-    @test_nowarn Renderer.Fast.test(false)
-    @test_nowarn Renderer.Fast.test()
+@testset "Assertions" begin
+    SmartAsserts.set_enabled(false)
+
+    @test_nowarn Renderer.Ray()
+    @test_nowarn Renderer.Ray(direction = ones(Renderer.Point))
+
+    @test_throws AssertionError Renderer.Ray(direction = zeros(Renderer.Point))
+    @test_throws AssertionError Renderer.Ray(direction = Renderer.Point(0, 1, 1))
+    @test_throws AssertionError Renderer.Ray(direction = Renderer.Point(0.060774088, 0.039998293, 0.8023627))
 end
