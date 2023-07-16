669d1c320ca6a188b5e75a548b175026f7103fb3
Zentrik <Zentrik@users.noreply.github.com>
2023-07-16 11:56:31 +0100

    Increase number of samples in benchmark script

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -11,7 +11,7 @@ SUITE["RayTracingInOneWeekend"] = BenchmarkGroup(["test"])
 SUITE["RayTracingInOneWeekend"]["GPU: 1920/4p with 10 samples per pixel and max depth of 16"] = begin
     scene, spectrum_img, camera = setup(:GPU)
     test(:GPU)
-    @benchmarkable CUDA.@sync(invokelatest($(render!), spectrum_img, scene, camera; samples_per_pixel=10, parallel=:GPU)) seconds=25
+    @benchmarkable CUDA.@sync(invokelatest($(render!), spectrum_img, scene, camera; samples_per_pixel=10, parallel=:GPU)) seconds=25 samples=50_000
 end
 
 SUITE["RayTracingInOneWeekend"]["GPU: 1920p with 100 samples per pixel and max depth of 16, requires multiple buffers"] = begin
