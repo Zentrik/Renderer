31aada3063aff04fd24690eae4925e458afd8b81
Zentrik <Zentrik@users.noreply.github.com>
2023-07-16 11:04:34 +0100

    Remove assertions from benchmarking script

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,18 +1,21 @@
 using BenchmarkTools
-using Renderer, CUDA
+macro smart_assert(ex, msg=nothing)
+    return :($(esc(ex)))
+end
+include("../src/RayTracingInOneWeekend.jl")
 
 const SUITE = BenchmarkGroup()
 
 SUITE["RayTracingInOneWeekend"] = BenchmarkGroup(["test"])
 
 SUITE["RayTracingInOneWeekend"]["GPU: 1920/4p with 10 samples per pixel and max depth of 16"] = begin
-    scene, spectrum_img, camera = Renderer.setup(:GPU)
-    Renderer.test(:GPU)
-    @benchmarkable CUDA.@sync(invokelatest($(Renderer.render!), spectrum_img, scene, camera; samples_per_pixel=10, parallel=:GPU)) seconds=25
+    scene, spectrum_img, camera = setup(:GPU)
+    test(:GPU)
+    @benchmarkable CUDA.@sync(invokelatest($(render!), spectrum_img, scene, camera; samples_per_pixel=10, parallel=:GPU)) seconds=25
 end
 
 SUITE["RayTracingInOneWeekend"]["GPU: 1920p with 100 samples per pixel and max depth of 16, requires multiple buffers"] = begin
-    scene, spectrum_img, camera = Renderer.setup(:GPU, 1920)
-    Renderer.test(:GPU)
-    @benchmarkable CUDA.@sync(invokelatest($(Renderer.render!), spectrum_img, scene, camera; samples_per_pixel=100, parallel=:GPU)) seconds=150
+    scene, spectrum_img, camera = setup(:GPU, 1920)
+    test(:GPU)
+    @benchmarkable CUDA.@sync(invokelatest($(render!), spectrum_img, scene, camera; samples_per_pixel=100, parallel=:GPU)) seconds=150
 end
