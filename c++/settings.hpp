980adbf0773dc9544abdb3c34c068cfcd9aa6689
Zentrik <Zentrik@users.noreply.github.com>
2023-11-08 06:16:49 +0000

    Remove submodules and have tests pass

--- c++/settings.hpp
+++
@@ -1,6 +0,0 @@
-// need to install tbb for this if using for_each, otherwise need to install OpenMP
-#define MULTITHREAD // Broken on Visual Studio
-// tbb broken on windows?
-
-// #define CLAFORTE
-#define PROFVIEW
